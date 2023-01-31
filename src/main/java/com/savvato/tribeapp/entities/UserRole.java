8057815bc4fba60f2c4946bf1dbfd073fdff5c00
Maryam Khan <maryam.khan@pcc.edu>
2023-01-31 14:37:46 -0800

    Added PHRASE_REVIEWER role to db and updated UserRole, UserRoleMapService

--- src/main/java/com/savvato/tribeapp/entities/UserRole.java
+++ src/main/java/com/savvato/tribeapp/entities/UserRole.java
@@ -10,7 +10,8 @@ public class UserRole {
 
 	public static final UserRole ROLE_ADMIN = new UserRole(1L, "ROLE_admin");
 	public static final UserRole ROLE_ACCOUNTHOLDER = new UserRole(2L, "ROLE_accountholder");
-	
+	public static final UserRole ROLE_PHRASEREVIEWER = new UserRole(2L, "ROLE_phrasereviewer");
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;

4d14cbe7580329ba8b29556b407753f8fda6db17
Maryam Khan <maryam.khan@pcc.edu>
2023-01-31 17:59:11 -0800

    Fixed bugs in previous commit

--- src/main/java/com/savvato/tribeapp/entities/UserRole.java
+++ src/main/java/com/savvato/tribeapp/entities/UserRole.java
@@ -10,7 +10,7 @@ public class UserRole {
 
 	public static final UserRole ROLE_ADMIN = new UserRole(1L, "ROLE_admin");
 	public static final UserRole ROLE_ACCOUNTHOLDER = new UserRole(2L, "ROLE_accountholder");
-	public static final UserRole ROLE_PHRASEREVIEWER = new UserRole(2L, "ROLE_phrasereviewer");
+	public static final UserRole ROLE_PHRASEREVIEWER = new UserRole(3L, "ROLE_phrasereviewer");
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)

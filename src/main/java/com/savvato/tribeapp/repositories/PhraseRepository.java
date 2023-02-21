2b6dea816b9eb97d4673ef728a3aa55cb774c2f6
Maryam Khan <maryam.khan@pcc.edu>
2023-02-21 15:45:30 -0800

    Added phraseRepository method to retrieve next to-be-reviewed phrase record from database

--- src/main/java/com/savvato/tribeapp/repositories/PhraseRepository.java
+++ src/main/java/com/savvato/tribeapp/repositories/PhraseRepository.java
@@ -1,9 +1,16 @@
 package com.savvato.tribeapp.repositories;
 
 import com.savvato.tribeapp.entities.Phrase;
+import com.savvato.tribeapp.entities.User;
 import com.savvato.tribeapp.entities.UserRoleMap;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface PhraseRepository extends CrudRepository<Phrase, Long> {
-
+    @Query(nativeQuery = true, value = "select phrase.* from to_be_reviewed phrase where phrase.id>?1 and phrase.hasBeenGroomed=1")
+    Optional<Phrase> findNextAvailablePhrase(Long id);
 }

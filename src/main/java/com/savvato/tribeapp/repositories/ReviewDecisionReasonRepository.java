b0565fe6a7ba8e97d4a5c8c187291f53e27a227e
Maryam Khan <maryam.khan@pcc.edu>
2023-03-02 15:53:37 -0800

    Added method for getting a ReviewDecisionReason record by the reason it was rejected

--- src/main/java/com/savvato/tribeapp/repositories/ReviewDecisionReasonRepository.java
+++ src/main/java/com/savvato/tribeapp/repositories/ReviewDecisionReasonRepository.java
@@ -4,6 +4,9 @@ import com.savvato.tribeapp.entities.ReviewDecisionReason;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReviewDecisionReasonRepository extends CrudRepository<ReviewDecisionReason, Long> {
+    Optional<ReviewDecisionReason> findByReason(String reason);
 }

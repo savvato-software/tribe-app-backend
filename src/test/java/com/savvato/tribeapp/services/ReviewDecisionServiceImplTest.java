a38b7515e3f4c87f575ab4349590023895199023
Maryam Khan <maryam.khan@pcc.edu>
2023-03-02 16:48:56 -0800

    Updated ReviewDecisionServiceImplTest to verify that at least 1 interaction occurs with ReviewDecisionReasonRepository and that the interaction passes in the value of the 'reason' parameter in ReviewDecisionService's saveReviewDecision() method

--- src/test/java/com/savvato/tribeapp/services/ReviewDecisionServiceImplTest.java
+++ src/test/java/com/savvato/tribeapp/services/ReviewDecisionServiceImplTest.java
@@ -1,2 +1,62 @@
-package com.savvato.tribeapp.services;public class ReviewDecisionServiceImplTest {
+package com.savvato.tribeapp.services;
+
+import com.savvato.tribeapp.controllers.dto.ReviewDecisionRequest;
+import com.savvato.tribeapp.entities.ReviewDecision;
+import com.savvato.tribeapp.entities.ReviewDecisionReason;
+import com.savvato.tribeapp.repositories.ReviewDecisionReasonRepository;
+import com.savvato.tribeapp.repositories.ReviewDecisionRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(SpringExtension.class)
+
+public class ReviewDecisionServiceImplTest {
+    @TestConfiguration
+    static class ReviewDecisionServiceTestContextConfiguration {
+        @Bean
+        public ReviewDecisionService reviewDecisionService() {
+            return new ReviewDecisionServiceImpl();
+        }
+    }
+
+    @Autowired
+    ReviewDecisionService reviewDecisionService;
+
+    @MockBean
+    ReviewDecisionRepository reviewDecisionRepository;
+    @MockBean
+    ReviewDecisionReasonRepository reviewDecisionReasonRepository;
+
+    @Test
+    public void saveReviewDecision() {
+        ReviewDecisionRequest reviewDecisionRequest = new ReviewDecisionRequest();
+        reviewDecisionRequest.reviewId = 1L;
+        reviewDecisionRequest.reviewerId = 2L;
+        reviewDecisionRequest.decision = "approved";
+
+        ReviewDecisionReason reviewDecisionReason = new ReviewDecisionReason();
+        reviewDecisionReason.setId(1L);
+        reviewDecisionReason.setReason("approved");
+        Mockito.when(reviewDecisionReasonRepository.findByReason(Mockito.any())).thenReturn(Optional.of(reviewDecisionReason));
+
+        reviewDecisionService.saveReviewDecision(reviewDecisionRequest.reviewId, reviewDecisionRequest.reviewerId, reviewDecisionRequest.decision);
+
+        ArgumentCaptor<String> arg1 = ArgumentCaptor.forClass(String.class);
+        verify(reviewDecisionReasonRepository, times(1)).findByReason(arg1.capture());
+        assertEquals(arg1.getValue(), reviewDecisionReason.getReason());
+    }
 }

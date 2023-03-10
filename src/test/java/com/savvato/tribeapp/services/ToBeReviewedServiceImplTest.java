92a331f8ca4db70abd165ed1a8d4dec9983be66d
Audra Wathen <mrsbluerose@gmail.com>
2023-03-10 10:22:57 -0700

    removes unnecessary import injectmocks

--- src/test/java/com/savvato/tribeapp/services/ToBeReviewedServiceImplTest.java
+++ src/test/java/com/savvato/tribeapp/services/ToBeReviewedServiceImplTest.java
@@ -4,7 +4,6 @@ import com.savvato.tribeapp.entities.ToBeReviewed;
 import com.savvato.tribeapp.repositories.ToBeReviewedRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;

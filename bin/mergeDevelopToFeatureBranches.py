bcc73becc8756f0f10bec40b637a0b417b354f03
Johnathan James <haxwell@gmail.com>
2023-03-18 18:01:24 -0600

    Added script to pull develop branch, and merge it to each feature branch automatically.

---
+++ bin/mergeDevelopToFeatureBranches.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+
+# Get the current branch name
+original_branch = os.popen('git rev-parse --abbrev-ref HEAD').read().strip()
+print("Current branch is: " + original_branch)
+
+# Stash any changes if present
+stash_output = os.popen('git stash').read()
+stashed_changes = len(stash_output.split('\n')) > 1
+if stashed_changes:
+	print("Stashed changes.")
+
+# Check out the develop branch and do a pull
+os.system('git checkout develop && git pull')
+print("Checked out develop branch and did a pull.")
+
+# Find the list of feature branches
+feature_branches = os.popen('git branch -r --list \*feature*').read().split('\n')
+feature_branches = [x.replace('origin/','') for x in feature_branches]
+
+# For each feature branch, merge the develop branch into it and push
+for feature_branch in feature_branches:
+	os.system('git checkout ' + feature_branch + ' && git merge develop && git push')
+	print("Merged develop branch into " + feature_branch + " and pushed.")
+
+# Change back to the original branch and pop any stashed changes
+os.system('git checkout ' + original_branch)
+print("Changed back to the original branch.")
+if stashed_changes:
+	os.system('git stash pop')
+	print("Popped any stashed changes.")
+

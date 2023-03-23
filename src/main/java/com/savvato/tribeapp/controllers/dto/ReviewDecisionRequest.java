5f636acf5331427d3918845cb9e71e8868a06c78
Maryam Khan <maryam.khan@pcc.edu>
2023-03-23 14:27:22 -0700

    Revert "Merge branch 'savvato-software:develop' into trib-59-test"
    This reverts commit 33d31cc331c6a5548e459a989a82242ecc8a7a7e, reversing
    changes made to 9678defad7f61f93c7a0cfde9c7ab8b6774c66f5.

--- src/main/java/com/savvato/tribeapp/controllers/dto/ReviewDecisionRequest.java
+++
@@ -1,7 +0,0 @@
-package com.savvato.tribeapp.controllers.dto;
-
-public class ReviewDecisionRequest {
-    public Long reviewId;
-    public Long reviewerId;
-    public Long reasonId;
-}
--- src/main/java/com/savvato/tribeapp/services/ReviewDecisionService.java
+++
@@ -1,8 +0,0 @@
-package com.savvato.tribeapp.services;
-import com.savvato.tribeapp.entities.ReviewDecision;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ReviewDecisionService {
-    ReviewDecision saveReviewDecision(Long reviewId, Long userId, Long reasonId);
-}

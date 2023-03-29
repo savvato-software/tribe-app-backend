fd5ca0c59821192538029665c850266eb7d6e184
Audra Wathen <mrsbluerose@gmail.com>
2023-03-29 12:01:53 -0600

    TRIB_33: adds new UserPhrase repository

---
+++ src/main/java/com/savvato/tribeapp/repositories/UserPhraseRepository.java
@@ -0,0 +1,15 @@
+package com.savvato.tribeapp.repositories;
+
+import com.savvato.tribeapp.entities.UserPhrase;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserPhraseRepository extends CrudRepository<UserPhrase, Long> {
+    @Query(nativeQuery = true, value = "select phrase_id from user_phrase where user_id = ?")
+    Optional<List<Long>> findPhraseIdsByUserId(Long Id);
+}

ff480561b159c94e49f5347fa2d72ec795277163
Audra Wathen <mrsbluerose@gmail.com>
2023-03-28 19:34:51 -0600

    adds migration script for phrase fk
    updates word entities

--- src/main/java/com/savvato/tribeapp/entities/RejectedNonEnglishWord.java
+++ src/main/java/com/savvato/tribeapp/entities/RejectedNonEnglishWord.java
@@ -4,7 +4,29 @@ import javax.persistence.*;
 
 @Entity
 @Table(name="rejected_non_english_word")
-public class RejectedNonEnglishWord extends AbstractWordEntity {
+public class RejectedNonEnglishWord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private String word;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
 
     public RejectedNonEnglishWord(String word) {
         this.setWord(word);

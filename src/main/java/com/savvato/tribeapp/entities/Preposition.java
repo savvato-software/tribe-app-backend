858073cb3d9149c3d8d63ff11e01bf0287fdc990
Audra Wathen <mrsbluerose@gmail.com>
2023-03-29 17:50:18 -0600

    TRIB_33-temp-with-word-fixes: adds correct word logic

--- src/main/java/com/savvato/tribeapp/entities/Preposition.java
+++ src/main/java/com/savvato/tribeapp/entities/Preposition.java
@@ -1,11 +1,32 @@
 package com.savvato.tribeapp.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="preposition")
-public class Preposition extends AbstractWordEntity {
+public class Preposition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private String word;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
 
     public Preposition(String word) {
         this.setWord(word);

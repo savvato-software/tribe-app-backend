fb24a548a3703f12d100b2fc31ff625bb8b18efb
Audra Wathen <mrsbluerose@gmail.com>
2023-03-30 18:07:01 -0600

    TRIB_33-temp: updates userPhraseService to have findPhraseIdsByUserId

--- src/main/java/com/savvato/tribeapp/services/UserPhraseService.java
+++ src/main/java/com/savvato/tribeapp/services/UserPhraseService.java
@@ -1,11 +1,8 @@
 package com.savvato.tribeapp.services;
 
-import com.savvato.tribeapp.entities.Phrase;
-import com.savvato.tribeapp.entities.UserPhrase;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface UserPhraseService {
-    Optional<List<Phrase>> findPhrasesByUserId(Long userId);
+    Optional<List<Long>> findPhraseIdsByUserId(Long userId);
 }
--- src/main/java/com/savvato/tribeapp/services/UserPhraseServiceImpl.java
+++ src/main/java/com/savvato/tribeapp/services/UserPhraseServiceImpl.java
@@ -1,5 +1,6 @@
 package com.savvato.tribeapp.services;
 
+import com.savvato.tribeapp.dto.PhraseDTO;
 import com.savvato.tribeapp.entities.Phrase;
 import com.savvato.tribeapp.repositories.UserPhraseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,31 +20,13 @@ public class UserPhraseServiceImpl implements UserPhraseService{
     PhraseServiceImpl phraseService;
 
     @Override
-    public Optional<List<Phrase>> findPhrasesByUserId(Long userId) {
-
-        // variable to hold list of all user phrases that will be returned
-        List<Phrase> phrases = new ArrayList<>();
+    public Optional<List<Long>> findPhraseIdsByUserId(Long userId) {
 
         // access user phrase repo to get a list of all phrase ids associated with user
         Optional<List<Long>> optPhraseIdsForUser = userPhraseRepository.findPhraseIdsByUserId(userId);
 
-        if (optPhraseIdsForUser.isPresent()){
-            // Loop through list of phrase ids and call phrase service to build phrase
-            List<Long> phraseIds = optPhraseIdsForUser.get();
-            for(Long phraseId : phraseIds){
-                Optional<Phrase> optCompletePhrase = phraseService.getPhraseWords(phraseId);
-                if(optCompletePhrase.isPresent()) {
-                    Phrase phrase = optCompletePhrase.get();
-                    phrases.add(phrase);
-                }
-            }
-            // ? Do we still use Optional? could there be no results? The enclosing if statement checks for any phrase IDs. If there are IDs, then the call to the phrase service should be guaranteed to return at least one phrase.
-            return Optional.of(phrases);
-
-            //do we need else? if we just return optional of phrases and there aren't any phrases, would it automatically be empty?
-        } else {
-            return Optional.empty();
-        }
+        return (optPhraseIdsForUser.isPresent() ? optPhraseIdsForUser : Optional.empty());
 
     }
+
 }

e253e1829d35bbf59c869ce6b2ff24bc0e83400f
Audra Wathen <mrsbluerose@gmail.com>
2023-03-30 18:00:00 -0600

    TRIB_33-temp: adds find phrase by phrase ID

--- src/main/java/com/savvato/tribeapp/repositories/PhraseRepository.java
+++ src/main/java/com/savvato/tribeapp/repositories/PhraseRepository.java
@@ -2,12 +2,16 @@ package com.savvato.tribeapp.repositories;
 
 
 import com.savvato.tribeapp.entities.Phrase;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PhraseRepository extends CrudRepository<Phrase, Long> {
 
-
+    @Query(nativeQuery = true, value = "select * from phrase where id = ?")
+    Optional<Phrase> findPhraseByPhraseId(Long Id);
 }
 

6bc9f212086e0a167b68a1242ff03dbc412eb491
Audra Wathen <mrsbluerose@gmail.com>
2023-04-18 12:16:43 -0600

    TRIB-73-check-first: checks if phrase is already mapped to user

--- src/main/java/com/savvato/tribeapp/entities/UserPhraseId.java
+++ src/main/java/com/savvato/tribeapp/entities/UserPhraseId.java
@@ -5,4 +5,19 @@ public class UserPhraseId implements Serializable {
     private Long userId;
     private Long phraseId;
 
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getPhraseId() {
+        return phraseId;
+    }
+
+    public void setPhraseId(Long phraseId) {
+        this.phraseId = phraseId;
+    }
 }

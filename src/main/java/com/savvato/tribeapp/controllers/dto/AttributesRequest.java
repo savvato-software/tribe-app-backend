a0bad5f23b9ab7cbac0db0fcdfe2c66b3d131ee8
Audra Wathen <mrsbluerose@gmail.com>
2023-04-19 09:30:14 -0600

    TRIB-73: updates AttributesAPIController > applyPhraseToUser > call do phraseService.applyPhraseToUser id parameter to userId

--- src/main/java/com/savvato/tribeapp/controllers/dto/AttributesRequest.java
+++ src/main/java/com/savvato/tribeapp/controllers/dto/AttributesRequest.java
@@ -3,7 +3,7 @@ package com.savvato.tribeapp.controllers.dto;
 // hold when data is coming in to the request
 public class AttributesRequest {
 
-    public Long id;
+    public Long userId;
     public String noun;
     public String preposition;
     public String verb;

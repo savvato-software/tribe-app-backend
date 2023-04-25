bb9f2475b6b35287723f7956ad94cc04d0ed3071
Audra Wathen <mrsbluerose@gmail.com>
2023-04-25 15:35:54 -0600

    TRIB-74: adds query to insert record

--- src/main/java/com/savvato/tribeapp/repositories/ReviewSubmittingUserRepository.java
+++ src/main/java/com/savvato/tribeapp/repositories/ReviewSubmittingUserRepository.java
@@ -1,6 +1,7 @@
 package com.savvato.tribeapp.repositories;
 
 import com.savvato.tribeapp.entities.ReviewSubmittingUser;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,8 @@ import org.springframework.stereotype.Repository;
 public interface ReviewSubmittingUserRepository extends CrudRepository<ReviewSubmittingUser, Long> {
     @Query(nativeQuery = true, value="delete from review_submitting_user rsu where rsu.toBeReviewedId=?1")
     Long deleteByToBeReviewedId(Long toBeReviewedId);
+
+    @Modifying
+    @Query(nativeQuery = true, value="INSERT INTO review_submitting_user (userId, toBeReviewedId) VALUES (?,?)")
+    Long saveUserAndReview(Long userId, Long toBeReviewedId);
 }

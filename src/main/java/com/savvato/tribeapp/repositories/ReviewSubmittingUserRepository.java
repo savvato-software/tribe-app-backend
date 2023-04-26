8d458b4ccdd2c290d045453c41a92f50f073b308
Audra Wathen <mrsbluerose@gmail.com>
2023-04-25 18:14:51 -0600

    TRIB-74: removes unnecessary import

--- src/main/java/com/savvato/tribeapp/repositories/ReviewSubmittingUserRepository.java
+++ src/main/java/com/savvato/tribeapp/repositories/ReviewSubmittingUserRepository.java
@@ -1,7 +1,6 @@
 package com.savvato.tribeapp.repositories;
 
 import com.savvato.tribeapp.entities.ReviewSubmittingUser;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;

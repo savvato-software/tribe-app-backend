ffb3424c68569ef0abdce9bffbf34009af320a14
loretdemolas <122843014+loretdemolas@users.noreply.github.com>
2023-05-30 20:09:38 -0400

    Update NotificationDTO.java
    Changed DTO from private to public.

--- src/main/java/com/savvato/tribeapp/dto/NotificationDTO.java
+++ src/main/java/com/savvato/tribeapp/dto/NotificationDTO.java
@@ -1,13 +1,14 @@
 package com.savvato.tribeapp.dto;
 
-import java.time.LocalDateTime;
+import lombok.Builder;
 
+@Builder
 
 public class NotificationDTO {
-    private String description;
-    private String body;
-    private String lastUpdatedDate;
-    private String iconUrl;
+    public String description;
+    public String body;
+    public String lastUpdatedDate;
+    public String iconUrl;
 
     public NotificationDTO(String description, String body, String lastUpdatedDate, String iconUrl) {
         this.description = description;
@@ -15,37 +16,4 @@ public class NotificationDTO {
         this.lastUpdatedDate = lastUpdatedDate;
         this.iconUrl = iconUrl;
     }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public String getLastUpdatedDate() {
-        return lastUpdatedDate;
-    }
-
-    public void setLastUpdatedDate(String lastUpdatedDate) {
-        this.lastUpdatedDate = lastUpdatedDate;
-    }
-
-    public String getIconUrl() {
-        return iconUrl;
-    }
-
-    public void setIconUrl(String iconUrl) {
-        this.iconUrl = iconUrl;
-    }
-// getters and setters
 }

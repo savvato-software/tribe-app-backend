c1254b197539cfa973d5a5d5e0db4c9a8944b314
Maryam Khan <kmaryam818@gmail.com>
2023-07-20 12:06:59 -0700

    Implemented websocket to handle connection-related communication between frontend and backend (#84)
    * Added websocket to facilitate real-time communication between backend & frontend during connection process
    
    * Added test for ConnectService's connect method when QR code is invalid
    
    * Added ConnectServiceImpl's validateQRCode method to ConnectService so it could be stubbed in testing
    
    * Slightly optimized connectWhenQrCodeIsValid test
    
    * Added unit test for when QR code is valid, but no connection intent is established yet
    
    * Simplified conditional logic in ConnectServiceImpl's handleConnectionIntent method
    
    * Added ConnectServiceImpl's saveConnectionDetails method to ConnectService for stubbing in tests
    
    * Added unit test for connect method's behavior when connection intent is confirmed and database save is successful
    
    * Realized some tests were unnecessary so removed them, and added test for handleConnectionIntent when connection intent isn't confirmed
    
    * Added unit test for handleConnectionIntent behavior when connection intent is confirmed and database save is successful
    
    * Modified saveConnectionDetails method to allow Optional to wrap null values
    
    * Modified saveConnectionDetails to use try-catch block when saving connection to database in order to catch the exception(s) it might throw
    
    * Added unit test for handling connection intent when connection intent is denied
    
    * Added unit tests for saveConnectionDetails and fixed minor errors in other tests that were causing false positives
    
    * Added MessageMapping for connect method to ensure websocket messages are routed correctly

--- src/main/java/com/savvato/tribeapp/services/ConnectServiceImpl.java
+++ src/main/java/com/savvato/tribeapp/services/ConnectServiceImpl.java
@@ -1,15 +1,23 @@
 package com.savvato.tribeapp.services;
 
+import com.savvato.tribeapp.config.principal.UserPrincipal;
+import com.savvato.tribeapp.controllers.dto.ConnectRequest;
+import com.savvato.tribeapp.dto.ConnectIncomingMessageDTO;
+import com.savvato.tribeapp.dto.ConnectOutgoingMessageDTO;
 import com.savvato.tribeapp.entities.Connection;
 import com.savvato.tribeapp.repositories.ConnectionsRepository;
+import com.savvato.tribeapp.repositories.UserRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
+
 @Service
 public class ConnectServiceImpl implements ConnectService {
 
@@ -17,7 +25,13 @@ public class ConnectServiceImpl implements ConnectService {
     CacheService cache;
 
     @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
     ConnectionsRepository connectionsRepository;
+
+    @Autowired
+    UserRepository userRepository;
 
     private static final Log logger = LogFactory.getLog(ConnectServiceImpl.class);
 
@@ -49,17 +63,69 @@ public class ConnectServiceImpl implements ConnectService {
         return new String(digits);
     }
 
-    public boolean connect(Long requestingUserId, Long toBeConnectedWithUserId, String qrcodePhrase) {
-        if (qrcodePhrase == getQRCodeString(toBeConnectedWithUserId).get()) {
-            Optional<Connection> opt = Optional.of(connectionsRepository.save(new Connection(requestingUserId, toBeConnectedWithUserId)));
+    public boolean saveConnectionDetails(Long requestingUserId, Long toBeConnectedWithUserId) {
+        Optional<Connection> opt;
+        try {
+            connectionsRepository.save(new Connection(requestingUserId, toBeConnectedWithUserId));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
 
-            if (opt.isPresent()) {
-                return true;
+    }
+
+    public Boolean validateQRCode(String qrcodePhrase, Long toBeConnectedWithUserId) {
+        return qrcodePhrase.equals(getQRCodeString(toBeConnectedWithUserId).get());
+    }
+
+    @MessageMapping("/connect/room")
+    public void connect(ConnectIncomingMessageDTO incoming, UserPrincipal user) {
+        if(!validateQRCode(incoming.qrcodePhrase, incoming.toBeConnectedWithUserId)) {
+            ConnectOutgoingMessageDTO msg = ConnectOutgoingMessageDTO.builder()
+                                            .connectionError(true)
+                                            .message("Invalid QR code; failed to connect.")
+                                            .build();
+            simpMessagingTemplate.convertAndSendToUser(
+                    String.valueOf(incoming.toBeConnectedWithUserId),
+                    "/connect/user/queue/specific-user",
+                    msg);
+        } else {
+            ConnectOutgoingMessageDTO outgoingMsg = handleConnectionIntent(incoming.connectionIntent, incoming.requestingUserId, incoming.toBeConnectedWithUserId);
+            for (Long userId : outgoingMsg.to) {
+                simpMessagingTemplate.convertAndSendToUser(
+                        String.valueOf(userId),
+                        "/connect/user/queue/specific-user",
+                        outgoingMsg);
             }
-            return false;
         }
-        else {
-            return false;
+    }
+
+    public ConnectOutgoingMessageDTO handleConnectionIntent(String connectionIntent, Long requestingUserId, Long toBeConnectedWithUserId) {
+        if (connectionIntent == "") {
+            List<Long> recipients = new ArrayList<>(Arrays.asList(toBeConnectedWithUserId));
+            return ConnectOutgoingMessageDTO.builder().message("Please confirm that you wish to connect.").to(recipients).build();
+        } else if (connectionIntent == "confirmed") {
+            Boolean connectionStatus = saveConnectionDetails(requestingUserId, toBeConnectedWithUserId);
+            List<Long> recipients = new ArrayList<>(Arrays.asList(requestingUserId, toBeConnectedWithUserId));
+            if (connectionStatus) {
+                return ConnectOutgoingMessageDTO.builder()
+                        .connectionSuccess(true)
+                        .to(recipients)
+                        .message("Successfully saved connection!").build();
+            } else {
+                return ConnectOutgoingMessageDTO.builder()
+                        .connectionError(true)
+                        .to(recipients)
+                        .message("Failed to save connection to database.").build();
+            }
+        } else if (connectionIntent == "denied") {
+            List<Long> recipients = new ArrayList<>(Arrays.asList(requestingUserId, toBeConnectedWithUserId));
+            return ConnectOutgoingMessageDTO.builder()
+                    .connectionError(true)
+                    .to(recipients)
+                    .message("Connection request denied.").build();
+
         }
+        return null;
     }
 }

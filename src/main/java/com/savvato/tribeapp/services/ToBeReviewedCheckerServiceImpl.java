7274848d28fa21a11352c10508a4001850f0cb5d
Audra Wathen <mrsbluerose@gmail.com>
2023-08-22 13:54:58 -0600

    TRIB-143: updates part of speech error type - updates the try/catch error type for a missing part of speech to be a null pointer exception

--- src/main/java/com/savvato/tribeapp/services/ToBeReviewedCheckerServiceImpl.java
+++ src/main/java/com/savvato/tribeapp/services/ToBeReviewedCheckerServiceImpl.java
@@ -83,7 +83,7 @@ public class ToBeReviewedCheckerServiceImpl implements ToBeReviewedCheckerServic
                     JsonObject definition = definitions.get(i).getAsJsonObject();
                     try {
                         partsOfSpeech.add(definition.get("partOfSpeech").getAsString());
-                    } catch (Exception e) {
+                    } catch (NullPointerException e) {
                         // Words API may occasionally have a null parts of speech. This is an error on their part.
                         log.warn(word + " is missing a parts of speech definition from the Words API. Set for manual review.");
                         return true;

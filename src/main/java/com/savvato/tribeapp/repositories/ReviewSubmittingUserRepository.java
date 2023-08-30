19e3a79b99523ffbff66f9bc5cdd23f14e8a97f0
Audra Wathen <mrsbluerose@gmail.com>
2023-08-29 19:45:21 -0600

    TRIB-143: removed uneccessary ID notation in repo - removed @ID annotation for ReviewSubmittingUserRepository

--- src/main/java/com/savvato/tribeapp/repositories/ReviewSubmittingUserRepository.java
+++ src/main/java/com/savvato/tribeapp/repositories/ReviewSubmittingUserRepository.java
@@ -1,7 +1,6 @@
 package com.savvato.tribeapp.repositories;
 
 import com.savvato.tribeapp.entities.ReviewSubmittingUser;
-import com.savvato.tribeapp.entities.ReviewSubmittingUserId;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.IdClass;
 
 @Repository
-@IdClass(ReviewSubmittingUserId.class)
 public interface ReviewSubmittingUserRepository extends CrudRepository<ReviewSubmittingUser, Long> {
 
     @Query(nativeQuery = true, value="select rsu.user_id from review_submitting_user rsu where rsu.to_be_reviewed_id=?1")

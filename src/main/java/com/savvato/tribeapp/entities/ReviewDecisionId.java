06bae906fac92864d7fe16b31c253727611e0792
Albey Kappil <50112186+albkappil@users.noreply.github.com>
2023-08-31 21:07:38 -0500

    backend changes for approve review attributes

--- src/main/java/com/savvato/tribeapp/entities/ReviewDecisionId.java
+++ src/main/java/com/savvato/tribeapp/entities/ReviewDecisionId.java
@@ -6,8 +6,10 @@ public class ReviewDecisionId implements Serializable {
     private Long reviewId;
     private Long userId;
 
+    public ReviewDecisionId() { }
     public ReviewDecisionId(Long reviewId, Long userId) {
         this.reviewId = reviewId;
         this.userId = userId;
     }
+
 }

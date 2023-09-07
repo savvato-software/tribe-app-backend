c5ef91e16c7721a5cafe8e474868ee1737dd5036
Audra Wathen <mrsbluerose@gmail.com>
2023-09-07 16:03:21 -0600

    TRIB-166: adds test for ReviewDecisionReason svc - adds test for ReviewDecisionReasonServiceImpl getReviewDecisionReasons method

--- src/test/java/com/savvato/tribeapp/services/ReviewDecisionReasonServiceImplTest.java
+++ src/test/java/com/savvato/tribeapp/services/ReviewDecisionReasonServiceImplTest.java
@@ -1,65 +1,57 @@
 package com.savvato.tribeapp.services;
 
-import com.savvato.tribeapp.controllers.dto.ReviewDecisionRequest;
-import com.savvato.tribeapp.entities.ReviewDecision;
+import com.savvato.tribeapp.dto.ReviewDecisionReasonDTO;
 import com.savvato.tribeapp.entities.ReviewDecisionReason;
 import com.savvato.tribeapp.repositories.ReviewDecisionReasonRepository;
-import com.savvato.tribeapp.repositories.ReviewDecisionRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(SpringExtension.class)
 
-public class ReviewDecisionServiceImplTest {
+public class ReviewDecisionReasonServiceImplTest {
     @TestConfiguration
-    static class ReviewDecisionServiceTestContextConfiguration {
+    static class ReviewDecisionReasonServiceTestContextConfiguration {
         @Bean
-        public ReviewDecisionService reviewDecisionService() {
-            return new ReviewDecisionServiceImpl();
+        public ReviewDecisionReasonService reviewDecisionReasonService() {
+            return new ReviewDecisionReasonServiceImpl();
         }
     }
 
     @Autowired
-    ReviewDecisionService reviewDecisionService;
+    ReviewDecisionReasonService reviewDecisionReasonService;
 
-    @MockBean
-    ReviewDecisionRepository reviewDecisionRepository;
     @MockBean
     ReviewDecisionReasonRepository reviewDecisionReasonRepository;
 
     @Test
-    public void saveReviewDecision() {
-        ReviewDecision decision = new ReviewDecision(1L, 1L, 1L);
-        Mockito.when(reviewDecisionRepository.save(Mockito.any())).thenReturn(decision);
-        ReviewDecision saveResult = reviewDecisionService.saveReviewDecision(decision.getReviewId(), decision.getUserId(), decision.getReasonId());
+    public void getReviewReasonDecisions() {
 
-        // Ensure that save() is called at least once on reviewDecisionRepository,
-        //  and that the arguments passed to save() match the given decision
-        ArgumentCaptor<ReviewDecision> arg1 = ArgumentCaptor.forClass(ReviewDecision.class);
-        verify(reviewDecisionRepository, times(1)).save(arg1.capture());
-        assertEquals(arg1.getValue().getReviewId(), decision.getReviewId());
-        assertEquals(arg1.getValue().getUserId(), decision.getUserId());
-        assertEquals(arg1.getValue().getReasonId(), decision.getReasonId());
+        List<ReviewDecisionReason> rdrList = new ArrayList<>();
 
-        // Ensure that the result returned by the save() function matches the given decision
-        assertEquals(saveResult.getReviewId(), decision.getReviewId());
-        assertEquals(saveResult.getUserId(), decision.getUserId());
-        assertEquals(saveResult.getReasonId(), decision.getReasonId());
+        for(int i=0; i<5; i++) {
+            ReviewDecisionReason rdr = new ReviewDecisionReason();
+            rdr.setId(Integer.toUnsignedLong(i+1));
+            rdr.setReason("testing" + i);
+            rdrList.add(rdr);
+        }
+
+        Mockito.when(reviewDecisionReasonRepository.findAllReviewDecisionReasons()).thenReturn(Optional.of(rdrList));
+        Optional<List<ReviewDecisionReasonDTO>> opt = reviewDecisionReasonService.getReviewDecisionReasons();
+        assertEquals(opt.get().size(), 5);
+        assertEquals(opt.get().get(4).id, 5L);
+        assertEquals(opt.get().get(4).reason, "testing4");
+
     }
 }

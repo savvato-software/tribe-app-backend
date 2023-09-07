6fda390625b0d1a600de64122ee6e75f86f8dbdb
Audra Wathen <mrsbluerose@gmail.com>
2023-09-07 14:24:27 -0600

    TRIB-166: adds ReviewDecisionReasonServiceImpl - adds ReviewDecisionReasonServiceImpl class - creates method get getReviewDecisionReasons

---
+++ src/main/java/com/savvato/tribeapp/services/ReviewDecisionReasonServiceImpl.java
@@ -0,0 +1,39 @@
+package com.savvato.tribeapp.services;
+
+import com.savvato.tribeapp.dto.ReviewDecisionReasonDTO;
+import com.savvato.tribeapp.entities.ReviewDecisionReason;
+import com.savvato.tribeapp.repositories.ReviewDecisionReasonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReviewDecisionReasonServiceImpl implements ReviewDecisionReasonService {
+
+    @Autowired
+    ReviewDecisionReasonRepository reviewDecisionReasonRepository;
+
+    @Override
+    public Optional<List<ReviewDecisionReasonDTO>> getReviewDecisionReasons() {
+
+        Optional<List<ReviewDecisionReason>> opt = reviewDecisionReasonRepository.findAllReviewDecisionReasons();
+
+        List<ReviewDecisionReasonDTO> rdrDtoList = new ArrayList<>();
+
+        if(opt.isPresent()){
+            List<ReviewDecisionReason> rdrList = opt.get();
+
+            for(ReviewDecisionReason rdr : rdrList) {
+                ReviewDecisionReasonDTO rdrDto = ReviewDecisionReasonDTO.builder().build();
+                rdrDto.reason = rdr.getReason();
+                rdrDtoList.add(rdrDto);
+            }
+        }
+
+        return Optional.of(rdrDtoList);
+    };
+
+}

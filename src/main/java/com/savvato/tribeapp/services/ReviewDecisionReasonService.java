fa7da7b6edddca43de9eb8711ee80538703146a9
Audra Wathen <mrsbluerose@gmail.com>
2023-09-11 14:53:56 -0600

    TRIB-166: removes unused import - removes unused import for Optional from ReviewDecisionReasonService

--- src/main/java/com/savvato/tribeapp/services/ReviewDecisionReasonService.java
+++ src/main/java/com/savvato/tribeapp/services/ReviewDecisionReasonService.java
@@ -4,7 +4,6 @@ import com.savvato.tribeapp.dto.ReviewDecisionReasonDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ReviewDecisionReasonService {
 

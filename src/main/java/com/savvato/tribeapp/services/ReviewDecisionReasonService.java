1e755871befaab15a0d3b2c313fb4b144cea22ac
Audra Wathen <mrsbluerose@gmail.com>
2023-09-11 14:52:16 -0600

    TRIB-166: removes annotation from service - removews @Service annotation from the ReviewDecisionReasonService interface

--- src/main/java/com/savvato/tribeapp/services/ReviewDecisionReasonService.java
+++ src/main/java/com/savvato/tribeapp/services/ReviewDecisionReasonService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public interface ReviewDecisionReasonService {
 
     List<ReviewDecisionReasonDTO> getReviewDecisionReasons();

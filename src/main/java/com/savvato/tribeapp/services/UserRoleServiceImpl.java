dcfd4d9e270b176a2062d0585d1a5d3f6d227a28
Audra Wathen <mrsbluerose@gmail.com>
2023-09-19 14:21:25 -0600

    TRIB-183: updates UserRoleService to return DTO - updates service/imple getRoles to return a list of type UserRoleDTO

--- src/main/java/com/savvato/tribeapp/services/UserRoleServiceImpl.java
+++ src/main/java/com/savvato/tribeapp/services/UserRoleServiceImpl.java
@@ -2,6 +2,7 @@
 
 package com.savvato.tribeapp.services;
 
+import com.savvato.tribeapp.dto.UserRoleDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,15 @@ public class UserRoleServiceImpl implements UserRoleService {
 	@Autowired
 	UserRoleRepository userRoleRepo;
 
-	public Iterable<UserRole> getRoles() {
-		return userRoleRepo.findAll();
+	public Iterable<UserRoleDTO> getRoles() {
+		Iterable<UserRole> userRoles = userRoleRepo.findAll();
+		List<UserRoleDTO> rtn = new ArrayList<>();
+		for (UserRole userRole : userRoles) {
+			UserRoleDTO userRoleDTO = UserRoleDTO.builder()
+					.name(userRole.getName())
+					.build();
+			rtn.add(userRoleDTO);
+		}
+		return rtn;
 	}
 }

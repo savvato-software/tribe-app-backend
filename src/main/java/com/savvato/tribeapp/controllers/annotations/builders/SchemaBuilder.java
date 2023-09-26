6dd962d76de1df42d00548978d1b86981c3be90f
maryamkhan14 <kmaryam818@gmail.com>
2023-09-25 19:08:40 -0700

    Create Builder class for OpenAPI schema model - Extend SwaggerCore's ComposedSchema class functionality to enable schema model merging

---
+++ src/main/java/com/savvato/tribeapp/controllers/annotations/builders/SchemaBuilder.java
@@ -0,0 +1,35 @@
+package com.savvato.tribeapp.controllers.annotations.builders;
+
+import com.savvato.tribeapp.controllers.annotations.utils.SchemaUtils;
+import io.swagger.v3.oas.models.media.ComposedSchema;
+import io.swagger.v3.oas.models.media.Schema;
+import lombok.NoArgsConstructor;
+
+import java.util.*;
+
+/** Builder class for OpenAPI schema model. */
+@NoArgsConstructor
+public class SchemaBuilder extends ComposedSchema {
+  private Schema<?> schema;
+
+  private Schema<?> getSchema() {
+    return this.schema;
+  }
+
+  private void setSchema(Schema<?> newSchema) {
+    schema = newSchema;
+  }
+
+  public Schema<?> build() {
+    if (schema != null && schema.getType() == null) schema.type("string");
+    return schema;
+  }
+
+  public void merge(Schema<?> patch) {
+    setSchema(SchemaUtils.mergeWithOverride(schema, patch));
+  }
+
+  public static SchemaBuilder builder() {
+    return new SchemaBuilder();
+  }
+}

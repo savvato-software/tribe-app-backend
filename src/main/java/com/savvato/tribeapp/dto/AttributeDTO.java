d9e1afc73d16596daabb3c70c58d1a84158ad75e
maryamkhan14 <kmaryam818@gmail.com>
2023-09-27 13:18:16 -0700

    Annotate response DTOs to generate corresponding OpenAPI models - Add @Schema annotations where necessary

--- src/main/java/com/savvato/tribeapp/dto/AttributeDTO.java
+++ src/main/java/com/savvato/tribeapp/dto/AttributeDTO.java
@@ -1,10 +1,12 @@
 package com.savvato.tribeapp.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 // hold when data is going out
+
+@Schema(description = "An attribute DTO")
 @Builder
 public class AttributeDTO {
-    public PhraseDTO phrase;
-
+  public PhraseDTO phrase;
 }

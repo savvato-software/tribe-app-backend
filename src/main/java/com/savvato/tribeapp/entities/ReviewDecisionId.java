a68ffa51948160c2543b3f3aafa6696b3c26c005
Albey Kappil <50112186+albkappil@users.noreply.github.com>
2023-10-05 19:20:34 -0500

    Remove constuctors

--- src/main/java/com/savvato/tribeapp/entities/ReviewDecisionId.java
+++ src/main/java/com/savvato/tribeapp/entities/ReviewDecisionId.java
@@ -6,10 +6,6 @@ public class ReviewDecisionId implements Serializable {
     private Long reviewId;
     private Long userId;
 
-    public ReviewDecisionId() { }
-    public ReviewDecisionId(Long reviewId, Long userId) {
-        this.reviewId = reviewId;
-        this.userId = userId;
-    }
+    
 
 }

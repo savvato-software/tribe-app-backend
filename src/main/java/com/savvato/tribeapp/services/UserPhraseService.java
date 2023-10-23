8b1cfaca8b393a060e90901b3dac609f9bdb755d
Audra Wathen <mrsbluerose@gmail.com>
2023-10-23 13:37:54 -0600

    TRIB-191: adds delete method to UserPhraseService

--- src/main/java/com/savvato/tribeapp/services/UserPhraseService.java
+++ src/main/java/com/savvato/tribeapp/services/UserPhraseService.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 public interface UserPhraseService {
     Optional<List<Long>> findPhraseIdsByUserId(Long userId);
 
+    void deletePhraseFromUser(Long phraseId, Long userId);
 }

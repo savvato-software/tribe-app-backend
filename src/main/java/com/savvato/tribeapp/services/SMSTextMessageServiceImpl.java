f1ab78d41eb738d1b290be52b9d296a6f02ea586
Maryam Khan <kmaryam818@gmail.com>
2023-10-31 16:50:23 -0700

    TRIB-148: Improvements necessary for adequate test coverage (#129)
    * Autowire RestTemplate in ToBeReviewedCheckerServiceImpl so it can be mocked in testing
    
    * Initialize Plivo separately in SMSTextMessageService so it can be mocked in testing
    
    * Exclude SystemTimeProviderImpl from coverage since it cannot be tested
    
    * Exclude main class and constants from coverage by default
    
    * Exclude documentation annotation-processing utilities from coverage
    
    * Mock RestTemplate in ToBeReviewedCheckerServiceImplTest

--- src/main/java/com/savvato/tribeapp/services/SMSTextMessageServiceImpl.java
+++ src/main/java/com/savvato/tribeapp/services/SMSTextMessageServiceImpl.java
@@ -1,52 +1,74 @@
 package com.savvato.tribeapp.services;
 
 import com.plivo.api.Plivo;
+import com.plivo.api.exceptions.PlivoRestException;
 import com.plivo.api.models.message.Message;
 import com.plivo.api.models.message.MessageCreateResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Service
 @Slf4j
 public class SMSTextMessageServiceImpl implements SMSTextMessageService {
 
-	@Autowired
-	CacheService cache;
-	
-	@Value("${PLIVO_SMS_AUTH_ID}")
+    @Autowired
+    CacheService cache;
+
+    @Value("${PLIVO_SMS_AUTH_ID}")
     private String SMS_API_AUTH_ID;
 
-	@Value("${PLIVO_SMS_AUTH_TOKEN}")
+    @Value("${PLIVO_SMS_AUTH_TOKEN}")
     private String SMS_API_AUTH_TOKEN;
 
-	public boolean sendSMS(String toPhoneNumber, String msg) {
-		boolean rtn = false;
 
-		if (toPhoneNumber.startsWith("0")) {
-			rtn = true; 	// for use in testing. just act like we did it.
+    public void initialize() {
 
-			log.debug("PRETENDED to send SMS to " + toPhoneNumber + " /  msg: [" + msg + "]");
-		} else {
-			try {
-				log.debug(SMS_API_AUTH_ID);
-				log.debug(SMS_API_AUTH_TOKEN);
-				Plivo.init(SMS_API_AUTH_ID, SMS_API_AUTH_TOKEN);
-				MessageCreateResponse response = Message.creator("19342227693", toPhoneNumber, msg).create();
+        Plivo.init(SMS_API_AUTH_ID, SMS_API_AUTH_TOKEN);
+    }
 
-				log.debug("sms sent to " + toPhoneNumber + " /  msg: [" + msg + "]");
-				log.debug("sms response msg: " + response.getMessage());
+    public Optional<MessageCreateResponse> createResponse(String toPhoneNumber, String msg) {
+        try {
+            MessageCreateResponse response = Message.creator("19342227693", toPhoneNumber, msg).create();
+            return Optional.of(response);
+        } catch (PlivoRestException plivoRestException) {
+            log.debug("PlivoRestException occurred when trying to create response: " + plivoRestException.getMessage());
+        } catch (IOException ioException) {
+            log.debug("IOException occurred when trying to create response: " + ioException.getMessage());
+        }
+        return Optional.empty();
+    }
 
-				rtn = true;
+    public boolean sendSMS(String toPhoneNumber, String msg) {
+        boolean rtn = false;
 
-			} catch (Exception e) {
-				log.info("Caught exception trying to send SMS,  ------ " + e.getMessage());
-			}
-		}
+        if (toPhoneNumber.startsWith("0")) {
+            rtn = true;    // for use in testing. just act like we did it.
 
-		return rtn;
-	}
+            log.debug("PRETENDED to send SMS to " + toPhoneNumber + " /  msg: [" + msg + "]");
+        } else {
+            try {
+                log.debug(SMS_API_AUTH_ID);
+                log.debug(SMS_API_AUTH_TOKEN);
+                initialize();
+                Optional<MessageCreateResponse> responseOpt = createResponse(toPhoneNumber, msg);
+                if (responseOpt.isPresent()) {
+                    MessageCreateResponse response = responseOpt.get();
+
+                    log.debug("sms sent to " + toPhoneNumber + " /  msg: [" + msg + "]");
+                    log.debug("sms response msg: " + response.getMessage());
+                    rtn = true;
+                }
+
+            } catch (Exception e) {
+                log.info("Caught exception trying to send SMS,  ------ " + e.getMessage());
+            }
+        }
+
+        return rtn;
+    }
 }

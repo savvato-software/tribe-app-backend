633ce6c925bb196f4ec2b2f5bbe8674665a2292a
maryamkhan14 <kmaryam818@gmail.com>
2023-11-02 16:05:15 -0700

    Create test suite for AttributesAPIController

---
+++ src/test/java/com/savvato/tribeapp/controllers/AttributesAPITest.java
@@ -0,0 +1,305 @@
+package com.savvato.tribeapp.controllers;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.savvato.tribeapp.config.principal.UserPrincipal;
+import com.savvato.tribeapp.constants.Constants;
+import com.savvato.tribeapp.controllers.dto.AttributesRequest;
+import com.savvato.tribeapp.dto.AttributeDTO;
+import com.savvato.tribeapp.dto.PhraseDTO;
+import com.savvato.tribeapp.entities.NotificationType;
+import com.savvato.tribeapp.entities.User;
+import com.savvato.tribeapp.entities.UserRole;
+import com.savvato.tribeapp.services.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AttributesAPIController.class)
+public class AttributesAPITest {
+    private UserPrincipal userPrincipal;
+    private User user;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthService authService;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private Gson gson;
+
+    @MockBean
+    private UserDetailsServiceTRIBEAPP userDetailsServiceTRIBEAPP;
+
+    @MockBean
+    private UserPrincipalService userPrincipalService;
+
+    @MockBean
+    private AttributesService attributesService;
+
+    @MockBean
+    private PhraseService phraseService;
+
+    @MockBean
+    private NotificationService notificationService;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        mockMvc =
+                MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+                        .apply(springSecurity())
+                        .build();
+
+        Set<UserRole> rolesSet = new HashSet<>();
+        rolesSet.add(UserRole.ROLE_ACCOUNTHOLDER);
+        rolesSet.add(UserRole.ROLE_ADMIN);
+        rolesSet.add(UserRole.ROLE_PHRASEREVIEWER);
+
+        user = new User();
+        user.setId(1L);
+        user.setName(Constants.FAKE_USER_NAME1);
+        user.setPassword("phrase_reviewer"); // pw => admin
+        user.setEnabled(1);
+        user.setRoles(rolesSet);
+        user.setCreated();
+        user.setLastUpdated();
+        user.setEmail(Constants.FAKE_USER_EMAIL1);
+    }
+
+    @Test
+    public void getAttributesForUserWhenAttributesFound() throws Exception {
+        Mockito.when(userPrincipalService.getUserPrincipalByEmail(Mockito.anyString()))
+                .thenReturn(new UserPrincipal(user));
+        String auth = AuthServiceImpl.generateAccessToken(user);
+        Long userId = 1L;
+        PhraseDTO phraseDTO =
+                PhraseDTO.builder()
+                        .verb("plays")
+                        .noun("chess")
+                        .adverb("competitively")
+                        .preposition("")
+                        .build();
+        AttributeDTO attributeDTO = AttributeDTO.builder().phrase(phraseDTO).build();
+        List<AttributeDTO> expectedAttributes = List.of(attributeDTO);
+        Optional<List<AttributeDTO>> opt = Optional.of(expectedAttributes);
+        when(attributesService.getAttributesByUserId(anyLong())).thenReturn(opt);
+        MvcResult result =
+                this.mockMvc
+                        .perform(
+                                get("/api/attributes/{userId}", userId)
+                                        .header("Authorization", "Bearer " + auth)
+                                        .characterEncoding("utf-8"))
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+        Type attributeDTOListType = new TypeToken<List<AttributeDTO>>() {
+        }.getType();
+
+        List<AttributeDTO> actualAttributes =
+                gson.fromJson(result.getResponse().getContentAsString(), attributeDTOListType);
+        assertThat(actualAttributes).usingRecursiveComparison().isEqualTo(expectedAttributes);
+    }
+
+    @Test
+    public void getAttributesForUserWhenNoAttributesFound() throws Exception {
+        Long userId = 1L;
+        Mockito.when(userPrincipalService.getUserPrincipalByEmail(Mockito.anyString()))
+                .thenReturn(new UserPrincipal(user));
+        String auth = AuthServiceImpl.generateAccessToken(user);
+        when(attributesService.getAttributesByUserId(anyLong())).thenReturn(Optional.empty());
+        this.mockMvc
+                .perform(
+                        get("/api/attributes/{userId}", userId)
+                                .header("Authorization", "Bearer " + auth)
+                                .characterEncoding("utf-8"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").doesNotExist()) // ensure body is empty
+                .andReturn();
+    }
+
+    @Test
+    public void applyPhraseToUserWhenPhraseValidAndApplicable() throws Exception {
+        Mockito.when(userPrincipalService.getUserPrincipalByEmail(Mockito.anyString()))
+                .thenReturn(new UserPrincipal(user));
+        String auth = AuthServiceImpl.generateAccessToken(user);
+        Long userId = 1L;
+        AttributesRequest attributesRequest = new AttributesRequest();
+        attributesRequest.userId = userId;
+        attributesRequest.id = 1L;
+        attributesRequest.adverb = "competitively";
+        attributesRequest.verb = "plays";
+        attributesRequest.noun = "chess";
+        attributesRequest.preposition = "";
+        String notificationContent = "Your attribute has been approved!";
+        when(phraseService.isPhraseValid(anyString(), anyString(), anyString(), anyString()))
+                .thenReturn(true);
+        when(phraseService.applyPhraseToUser(
+                anyLong(), anyString(), anyString(), anyString(), anyString()))
+                .thenReturn(true);
+        when(notificationService.createNotification(
+                any(NotificationType.class), anyLong(), anyString(), anyString()))
+                .thenReturn(null);
+        ArgumentCaptor<NotificationType> notificationTypeCaptor =
+                ArgumentCaptor.forClass(NotificationType.class);
+        ArgumentCaptor<Long> userIdCaptor = ArgumentCaptor.forClass(Long.class);
+        ArgumentCaptor<String> notificationTypeNameCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> notificationContentCaptor = ArgumentCaptor.forClass(String.class);
+
+        this.mockMvc
+                .perform(
+                        post("/api/attributes")
+                                .content(gson.toJson(attributesRequest))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + auth)
+                                .characterEncoding("utf-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"))
+                .andReturn();
+
+        verify(notificationService, times(1))
+                .createNotification(
+                        notificationTypeCaptor.capture(),
+                        userIdCaptor.capture(),
+                        notificationTypeNameCaptor.capture(),
+                        notificationContentCaptor.capture());
+        assertThat(notificationTypeCaptor.getValue()).usingRecursiveComparison().isEqualTo(NotificationType.ATTRIBUTE_REQUEST_APPROVED);
+        assertEquals(userIdCaptor.getValue(), userId);
+        assertEquals(
+                notificationTypeNameCaptor.getValue(),
+                NotificationType.ATTRIBUTE_REQUEST_APPROVED.getName());
+        assertEquals(notificationContentCaptor.getValue(), notificationContent);
+    }
+
+    @Test
+    public void applyPhraseToUserWhenPhraseValidButNotApplied() throws Exception {
+        Mockito.when(userPrincipalService.getUserPrincipalByEmail(Mockito.anyString()))
+                .thenReturn(new UserPrincipal(user));
+        String auth = AuthServiceImpl.generateAccessToken(user);
+        Long userId = 1L;
+        AttributesRequest attributesRequest = new AttributesRequest();
+        attributesRequest.userId = userId;
+        attributesRequest.id = 1L;
+        attributesRequest.adverb = "competitively";
+        attributesRequest.verb = "plays";
+        attributesRequest.noun = "chess";
+        attributesRequest.preposition = "";
+
+        when(phraseService.isPhraseValid(anyString(), anyString(), anyString(), anyString()))
+                .thenReturn(true);
+        when(phraseService.applyPhraseToUser(
+                anyLong(), anyString(), anyString(), anyString(), anyString()))
+                .thenReturn(false);
+        when(notificationService.createNotification(
+                any(NotificationType.class), anyLong(), anyString(), anyString()))
+                .thenReturn(null);
+        ArgumentCaptor<NotificationType> notificationTypeCaptor =
+                ArgumentCaptor.forClass(NotificationType.class);
+        ArgumentCaptor<Long> userIdCaptor = ArgumentCaptor.forClass(Long.class);
+        ArgumentCaptor<String> notificationTypeNameCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> notificationContentCaptor = ArgumentCaptor.forClass(String.class);
+        String notificationContent =
+                "Your attribute was rejected. This attribute is unsuitable and cannot be applied to users.";
+        this.mockMvc
+                .perform(
+                        post("/api/attributes")
+                                .content(gson.toJson(attributesRequest))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + auth)
+                                .characterEncoding("utf-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("false"))
+                .andReturn();
+
+        verify(notificationService, times(1))
+                .createNotification(
+                        notificationTypeCaptor.capture(),
+                        userIdCaptor.capture(),
+                        notificationTypeNameCaptor.capture(),
+                        notificationContentCaptor.capture());
+        assertThat(notificationTypeCaptor.getValue()).usingRecursiveComparison().isEqualTo(NotificationType.ATTRIBUTE_REQUEST_REJECTED);
+        assertEquals(userIdCaptor.getValue(), userId);
+        assertEquals(
+                notificationTypeNameCaptor.getValue(),
+                NotificationType.ATTRIBUTE_REQUEST_REJECTED.getName());
+        assertEquals(notificationContentCaptor.getValue(), notificationContent);
+    }
+
+    @Test
+    public void applyPhraseToUserWhenPhraseInvalid() throws Exception {
+        Mockito.when(userPrincipalService.getUserPrincipalByEmail(Mockito.anyString()))
+                .thenReturn(new UserPrincipal(user));
+        String auth = AuthServiceImpl.generateAccessToken(user);
+        Long userId = 1L;
+        AttributesRequest attributesRequest = new AttributesRequest();
+        attributesRequest.userId = userId;
+        attributesRequest.id = 1L;
+        attributesRequest.adverb = "competitively";
+        attributesRequest.verb = "plays";
+        attributesRequest.noun = "chess";
+        attributesRequest.preposition = "";
+
+        when(phraseService.isPhraseValid(anyString(), anyString(), anyString(), anyString()))
+                .thenReturn(false);
+        when(notificationService.createNotification(
+                any(NotificationType.class), anyLong(), anyString(), anyString()))
+                .thenReturn(null);
+        ArgumentCaptor<NotificationType> notificationTypeCaptor =
+                ArgumentCaptor.forClass(NotificationType.class);
+        ArgumentCaptor<Long> userIdCaptor = ArgumentCaptor.forClass(Long.class);
+        ArgumentCaptor<String> notificationTypeNameCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> notificationContentCaptor = ArgumentCaptor.forClass(String.class);
+        String notificationContent =
+                "Your attribute was rejected. This attribute is unsuitable and cannot be applied to users.";
+        this.mockMvc
+                .perform(
+                        post("/api/attributes")
+                                .content(gson.toJson(attributesRequest))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + auth)
+                                .characterEncoding("utf-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("false"))
+                .andReturn();
+        verify(phraseService, never())
+                .applyPhraseToUser(anyLong(), anyString(), anyString(), anyString(), anyString());
+        verify(notificationService, times(1))
+                .createNotification(
+                        notificationTypeCaptor.capture(),
+                        userIdCaptor.capture(),
+                        notificationTypeNameCaptor.capture(),
+                        notificationContentCaptor.capture());
+        assertThat(notificationTypeCaptor.getValue()).usingRecursiveComparison().isEqualTo(NotificationType.ATTRIBUTE_REQUEST_REJECTED);
+        assertEquals(userIdCaptor.getValue(), userId);
+        assertEquals(
+                notificationTypeNameCaptor.getValue(),
+                NotificationType.ATTRIBUTE_REQUEST_REJECTED.getName());
+        assertEquals(notificationContentCaptor.getValue(), notificationContent);
+    }
+}

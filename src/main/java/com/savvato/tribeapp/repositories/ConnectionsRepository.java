57d468850ed90549d778be41828623e1a104a3bf
Johnathan James <haxwell@gmail.com>
2023-12-13 15:21:53 -0700

    Added endpoint to return a list of all the connections for a user

--- src/main/java/com/savvato/tribeapp/repositories/ConnectionsRepository.java
+++ src/main/java/com/savvato/tribeapp/repositories/ConnectionsRepository.java
@@ -5,6 +5,9 @@ import com.savvato.tribeapp.entities.Noun;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ConnectionsRepository extends CrudRepository<Connection, Long> {
+    List<Connection> findAllByToBeConnectedWithUserId(Long toBeConnectedWithUserId);
 }

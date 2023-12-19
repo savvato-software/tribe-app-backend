4699b8ae55f46fe6069514574fe5fa2cacdbc66f
maryamkhan14 <kmaryam818@gmail.com>
2023-12-19 14:57:42 -0800

    Update endpoint documentation to reflect GenericMessageDTO usage

--- src/main/java/com/savvato/tribeapp/controllers/annotations/controllers/AttributesAPIController/GetNumberOfUsersWithAttribute.java
+++ src/main/java/com/savvato/tribeapp/controllers/annotations/controllers/AttributesAPIController/GetNumberOfUsersWithAttribute.java
@@ -2,6 +2,7 @@ package com.savvato.tribeapp.controllers.annotations.controllers.AttributesAPICo
 
 import com.savvato.tribeapp.controllers.annotations.responses.BadRequest;
 import com.savvato.tribeapp.controllers.annotations.responses.Success;
+import com.savvato.tribeapp.dto.GenericMessageDTO;
 import io.swagger.v3.oas.annotations.Operation;
 
 import java.lang.annotation.*;
@@ -14,8 +15,7 @@ import java.lang.annotation.*;
         description = "Provided an attribute ID, get the number of users that share this attribute.")
 @Success(
         description = "Successfully retrieved user attributes",
-        implementation = Integer.class,
-        example = "8")
+        implementation = GenericMessageDTO.class)
 @BadRequest(
         description = "Failed to retrieve number of users with this attribute",
         noContent = true)

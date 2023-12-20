1d2585fcba0cb663fba171c15c9d97b53593396c
maryamkhan14 <kmaryam818@gmail.com>
2023-12-19 17:09:32 -0800

    Add repository method for deleting connection from connections table

--- src/main/java/com/savvato/tribeapp/repositories/ConnectionsRepository.java
+++ src/main/java/com/savvato/tribeapp/repositories/ConnectionsRepository.java
@@ -1,10 +1,12 @@
 package com.savvato.tribeapp.repositories;
 
 import com.savvato.tribeapp.entities.Connection;
-import com.savvato.tribeapp.entities.Noun;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ConnectionsRepository extends CrudRepository<Connection, Long> {
+    @Query(nativeQuery = true, value = "delete from connections where requested_user_id=?1 AND to_be_connected_with_user_id=?2")
+    void removeConnection(Long requestingUserId, Long connectedWithUserId);
 }

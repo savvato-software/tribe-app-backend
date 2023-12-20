ff2ee34594982349f231de68d3630c6a77cf7eb7
maryamkhan14 <kmaryam818@gmail.com>
2023-12-19 17:12:54 -0800

    Create DELETE endpoint for removing a connection

--- src/main/java/com/savvato/tribeapp/controllers/ConnectAPIController.java
+++ src/main/java/com/savvato/tribeapp/controllers/ConnectAPIController.java
@@ -1,15 +1,13 @@
 package com.savvato.tribeapp.controllers;
 
-import com.savvato.tribeapp.config.principal.UserPrincipal;
 import com.savvato.tribeapp.controllers.annotations.controllers.ConnectAPIController.Connect;
 import com.savvato.tribeapp.controllers.annotations.controllers.ConnectAPIController.GetQRCodeString;
 import com.savvato.tribeapp.controllers.dto.ConnectRequest;
+import com.savvato.tribeapp.controllers.dto.ConnectionDeleteRequest;
 import com.savvato.tribeapp.dto.ConnectIncomingMessageDTO;
 import com.savvato.tribeapp.services.ConnectService;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.Optional;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,48 +16,57 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Optional;
+
 @RestController
 @Tag(name = "connect", description = "Connections between users")
 @RequestMapping("/api/connect")
 public class ConnectAPIController {
-  @Autowired ConnectService connectService;
+    @Autowired
+    ConnectService connectService;
 
-  ConnectAPIController() {}
-
-  @GetQRCodeString
-  @GetMapping("/{userId}")
-  public ResponseEntity getQrCodeString(
-      @Parameter(description = "The user ID of a user", example = "1") @PathVariable Long userId) {
-
-    Optional<String> opt = connectService.storeQRCodeString(userId);
-
-    if (opt.isPresent()) {
-      return ResponseEntity.status(HttpStatus.OK).body(opt.get());
-    } else {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    ConnectAPIController() {
     }
-  }
 
-  @Connect
-  @PostMapping
-  public boolean connect(@RequestBody @Valid ConnectRequest connectRequest) {
-    if (connectService.validateQRCode(
-        connectRequest.qrcodePhrase, connectRequest.toBeConnectedWithUserId)) {
-      boolean isConnectionSaved =
-          connectService.saveConnectionDetails(
-              connectRequest.requestingUserId, connectRequest.toBeConnectedWithUserId);
-      if (isConnectionSaved) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return false;
+    @GetQRCodeString
+    @GetMapping("/{userId}")
+    public ResponseEntity getQrCodeString(
+            @Parameter(description = "The user ID of a user", example = "1") @PathVariable Long userId) {
+
+        Optional<String> opt = connectService.storeQRCodeString(userId);
+
+        if (opt.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(opt.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
-  }
 
-  @MessageMapping("/connect/room")
-  public void connect(@Payload ConnectIncomingMessageDTO incoming, @Header("simpSessionId") String sessionId) {
-      connectService.connect(incoming);
-  }
+    @Connect
+    @PostMapping
+    public boolean connect(@RequestBody @Valid ConnectRequest connectRequest) {
+        if (connectService.validateQRCode(
+                connectRequest.qrcodePhrase, connectRequest.toBeConnectedWithUserId)) {
+            boolean isConnectionSaved =
+                    connectService.saveConnectionDetails(
+                            connectRequest.requestingUserId, connectRequest.toBeConnectedWithUserId);
+            return isConnectionSaved;
+        } else {
+            return false;
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Boolean> removeConnection(@RequestBody @Valid ConnectionDeleteRequest connectionDeleteRequest) {
+        if (connectService.removeConnection(connectionDeleteRequest)) {
+            return ResponseEntity.ok().body(true);
+        }
+        return ResponseEntity.badRequest().body(false);
+    }
+
+    @MessageMapping("/connect/room")
+    public void connect(@Payload ConnectIncomingMessageDTO incoming, @Header("simpSessionId") String sessionId) {
+        connectService.connect(incoming);
+    }
 }

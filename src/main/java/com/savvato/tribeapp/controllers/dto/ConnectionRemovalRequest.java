58c5bfe41d8cc855e04a04a6bbc47d8529c08494
maryamkhan14 <kmaryam818@gmail.com>
2024-01-02 16:42:32 -0800

    Rename ConnectionDeleteRequest to ConnectionRemovalRequest

--- src/main/java/com/savvato/tribeapp/controllers/dto/ConnectionRemovalRequest.java
+++ src/main/java/com/savvato/tribeapp/controllers/dto/ConnectionRemovalRequest.java
@@ -3,7 +3,7 @@ package com.savvato.tribeapp.controllers.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "A request to delete a connection between two users")
-public class ConnectionDeleteRequest {
+public class ConnectionRemovalRequest {
     @Schema(example = "1")
     public Long requestingUserId;
 

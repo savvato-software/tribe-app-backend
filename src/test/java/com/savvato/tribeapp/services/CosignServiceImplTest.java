ecd06c3e0d48f2be1c10ce257c4efcfb70814ddd
Audra Wathen <mrsbluerose@gmail.com>
2024-01-11 16:14:32 -0700

    TRIB-210: adds cosign service test for save cosign

--- src/test/java/com/savvato/tribeapp/services/CosignServiceImplTest.java
+++ src/test/java/com/savvato/tribeapp/services/CosignServiceImplTest.java
@@ -3,11 +3,15 @@ package com.savvato.tribeapp.services;
 import com.savvato.tribeapp.repositories.CosignRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith({SpringExtension.class})
 public class CosignServiceImplTest extends AbstractServiceImplTest{
@@ -30,6 +34,12 @@ public class CosignServiceImplTest extends AbstractServiceImplTest{
 
     @Test
     public void saveCosign() {
+        Long userIdIssuing = 1L;
+        Long userIdReceiving = 1L;
+        Long phraseId = 1L;
+
+        cosignService.saveCosign(userIdIssuing, userIdReceiving, phraseId);
+        verify(cosignRepository, times(1)).save(Mockito.any());
 
     }
 }

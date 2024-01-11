5444fba749c4d98bf129b4b5e57a44ef34a644fd
Audra Wathen <mrsbluerose@gmail.com>
2024-01-11 15:40:03 -0700

    TRIB-210: adds cosign service test class

---
+++ src/test/java/com/savvato/tribeapp/services/CosignServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.savvato.tribeapp.services;
+
+import com.savvato.tribeapp.repositories.CosignRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith({SpringExtension.class})
+public class CosignServiceImplTest extends AbstractServiceImplTest{
+
+    @TestConfiguration
+    static class CosignServiceImplTestContextConfiguration {
+
+        @Bean
+        public  CosignService cosignService() {
+            return new CosignServiceImpl();
+        }
+
+    }
+
+    @Autowired
+    CosignService cosignService;
+
+    @MockBean
+    CosignRepository cosignRepository;
+
+    @Test
+    public void saveCosign() {
+
+    }
+}

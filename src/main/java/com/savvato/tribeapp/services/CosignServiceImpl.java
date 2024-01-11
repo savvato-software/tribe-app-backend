bbf27d42aa4a2114e0aefb4928154c7a69214f92
Audra Wathen <mrsbluerose@gmail.com>
2024-01-11 12:55:40 -0700

    TRIB-210: adds Cosign service SaveCosign method

---
+++ src/main/java/com/savvato/tribeapp/services/CosignServiceImpl.java
@@ -0,0 +1,26 @@
+package com.savvato.tribeapp.services;
+
+import com.savvato.tribeapp.entities.Cosign;
+import com.savvato.tribeapp.repositories.CosignRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class CosignServiceImpl implements CosignService {
+
+    @Autowired
+    CosignRepository cosignRepository;
+
+    @Override
+    public void saveCosign(Long userIdIssuing, Long userIdReceiving, Long phraseId) {
+
+        Cosign cosign = new Cosign();
+        cosign.setUserIdIssuing(userIdIssuing);
+        cosign.setUserIdReceiving(userIdReceiving);
+        cosign.setPhraseId(phraseId);
+        cosignRepository.save(cosign);
+        log.info("Cosign from user: " + userIdIssuing + " to user: " + userIdReceiving + " added." );
+    }
+}

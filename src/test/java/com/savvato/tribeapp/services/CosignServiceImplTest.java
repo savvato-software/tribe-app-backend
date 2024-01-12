3e51a79f8a0a24a09657cdd79b2f9ec9278b72ee
Audra Wathen <mrsbluerose@gmail.com>
2024-01-12 11:01:13 -0700

    TRIB-210: updates cosign svc save test w/ rtn dto

--- src/test/java/com/savvato/tribeapp/services/CosignServiceImplTest.java
+++ src/test/java/com/savvato/tribeapp/services/CosignServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.savvato.tribeapp.services;
 
+import com.savvato.tribeapp.dto.CosignDTO;
+import com.savvato.tribeapp.entities.Cosign;
 import com.savvato.tribeapp.repositories.CosignRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,8 +12,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static net.bytebuddy.matcher.ElementMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith({SpringExtension.class})
 public class CosignServiceImplTest extends AbstractServiceImplTest{
@@ -38,8 +41,23 @@ public class CosignServiceImplTest extends AbstractServiceImplTest{
         Long userIdReceiving = 1L;
         Long phraseId = 1L;
 
-        cosignService.saveCosign(userIdIssuing, userIdReceiving, phraseId);
-        verify(cosignRepository, times(1)).save(Mockito.any());
+        Cosign cosign = new Cosign();
+        cosign.setUserIdIssuing(userIdIssuing);
+        cosign.setUserIdReceiving(userIdReceiving);
+        cosign.setPhraseId(phraseId);
 
+        CosignDTO cosignDTO = CosignDTO.builder().build();
+        cosignDTO.userIdIssuing = userIdIssuing;
+        cosignDTO.userIdReceiving = userIdReceiving;
+        cosignDTO.phraseId = phraseId;
+
+        when(cosignRepository.save(Mockito.any())).thenReturn(cosign);
+
+        CosignDTO expectedCosignDTO = cosignService.saveCosign(userIdIssuing, userIdReceiving, phraseId);
+
+        verify(cosignRepository, times(1)).save(Mockito.any());
+        assertEquals(cosignDTO.userIdIssuing, expectedCosignDTO.userIdIssuing);
+        assertEquals(cosignDTO.userIdReceiving, expectedCosignDTO.userIdReceiving);
+        assertEquals(cosignDTO.phraseId, expectedCosignDTO.phraseId);
     }
 }

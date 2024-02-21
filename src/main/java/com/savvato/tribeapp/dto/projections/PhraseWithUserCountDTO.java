8809b111c6ab71a497fe1f12f8f2eca113a771df
maryamkhan14 <kmaryam818@gmail.com>
2024-02-20 18:15:05 -0800

    Remove unnecessary equals() method override in projection DTO

--- src/main/java/com/savvato/tribeapp/dto/projections/PhraseWithUserCountDTO.java
+++ src/main/java/com/savvato/tribeapp/dto/projections/PhraseWithUserCountDTO.java
@@ -1,16 +1,7 @@
 package com.savvato.tribeapp.dto.projections;
 
 
-import java.util.Objects;
-
 public record PhraseWithUserCountDTO(Long id, Long adverbId, Long verbId, Long prepositionId, Long nounId,
                                      Long userCount) {
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PhraseWithUserCountDTO phraseWithUserCountDTO = (PhraseWithUserCountDTO) o;
-        return Objects.equals(id, phraseWithUserCountDTO.id) && Objects.equals(adverbId, phraseWithUserCountDTO.adverbId) && Objects.equals(verbId, phraseWithUserCountDTO.verbId) && Objects.equals(prepositionId, phraseWithUserCountDTO.prepositionId) && Objects.equals(nounId, phraseWithUserCountDTO.nounId);
-    }
 }

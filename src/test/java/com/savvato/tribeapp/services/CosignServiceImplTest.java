dd6e41675e16868bc3d4e055661d6edc0620ce01
Audra Wathen <mrsbluerose@gmail.com>
2024-03-06 15:08:55 -0700

    TRIB-236: adds test for cosign svc getCosigners

--- src/test/java/com/savvato/tribeapp/services/CosignServiceImplTest.java
+++ src/test/java/com/savvato/tribeapp/services/CosignServiceImplTest.java
@@ -1,8 +1,10 @@
 package com.savvato.tribeapp.services;
 
 import com.savvato.tribeapp.dto.CosignDTO;
+import com.savvato.tribeapp.dto.UserNameDTO;
 import com.savvato.tribeapp.entities.Cosign;
 import com.savvato.tribeapp.repositories.CosignRepository;
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -11,6 +13,9 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static net.bytebuddy.matcher.ElementMatchers.any;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -34,6 +39,9 @@ public class CosignServiceImplTest extends AbstractServiceImplTest{
 
     @MockBean
     CosignRepository cosignRepository;
+
+    @MockBean
+    UserService userService;
 
     @Test
     public void saveCosign() {
@@ -90,5 +98,39 @@ public class CosignServiceImplTest extends AbstractServiceImplTest{
         assertEquals(cosignDTO.userIdIssuing, expectedCosignDTORepeat.userIdIssuing);
         assertEquals(cosignDTO.userIdReceiving, expectedCosignDTORepeat.userIdReceiving);
         assertEquals(cosignDTO.phraseId, expectedCosignDTORepeat.phraseId);
+    }
+
+    @Test
+    public void testGetCosignersForUserAttributeHappyPath(){
+        // test data
+        Long testUserIdIssuing = 1L;
+        String testUserNameIssuing = "test";
+        Long testUserIdReceiving = 2L;
+        Long testPhraseId = 1L;
+
+        // mock return data
+        UserNameDTO userNameDTO = UserNameDTO.builder()
+                .userId(testUserIdIssuing)
+                .userName(testUserNameIssuing)
+                .build();
+
+        List<Long> expectedCosignerIds = new ArrayList<>();
+        expectedCosignerIds.add(testUserIdIssuing);
+
+        List<UserNameDTO> expectedListUserNameDTO = new ArrayList<>();
+        expectedListUserNameDTO.add(userNameDTO);
+
+        // mock returns
+        when(cosignRepository.findCosignersByUserIdReceivingAndPhraseId(anyLong(),anyLong())).thenReturn(expectedCosignerIds);
+
+        when(userService.getUserNameDTO(anyLong())).thenReturn(userNameDTO);
+
+        // test
+        List<UserNameDTO> userNameDTOS = cosignService.getCosignersForUserAttribute(testUserIdReceiving,testPhraseId);
+
+        for(UserNameDTO user : userNameDTOS){
+            assertEquals(user.userId, expectedListUserNameDTO.get(0).userId);
+            assertEquals(user.userName,expectedListUserNameDTO.get(0).userName);
+        }
     }
 }
